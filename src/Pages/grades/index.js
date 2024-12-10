49f48e0b583b6113d21f1cbe26a8ffea80274f1d
Radin Movahedi <radinmov@Radins-MacBook-Air.local>
2024-12-10 12:03:18 +0330

    grades ui update

--- src/Pages/grades/index.js
+++ src/Pages/grades/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Sidebar from '../../componets/Sidebar';
+import { FaQuestion  , FaChartBar} from 'react-icons/fa';
 import useTitle from '../../componets/Hook/useTitle';
 
 
@@ -8,7 +9,6 @@ export const Grades = () => {
   useTitle("Grades");
   const navigate = useNavigate();
 
-  // State to store scores
   const [scores, setScores] = useState({
     exam1: "",
     exam2: "",
@@ -35,46 +35,51 @@ export const Grades = () => {
           <h1 className="text-2xl font-bold mb-6">نمرات</h1>
           
           {/* Button to navigate to the grade chart page */}
-          <div className="bg-white shadow-md rounded-md p-6 mb-6">
+          <div className="w-[145px] h-[145px] bg-white shadow-md rounded-md p-6 mb-6">
             <button
-              className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
+              className="text-black px-4 py-2 rounded-md hover:bg-blue-600"
               onClick={handleViewCharts}
             >
               نمایش نمودار نمرات
             </button>
+              <FaChartBar size={50} color="blue" />
           </div>
 
           {/* Exam 1 */}
-          <div className="bg-white shadow-md rounded-md p-6 mb-4">
+          <div className="bg-white w-[174px] h-[172px] shadow-md  m-2 rounded-md p-6 mb-4">
+            <div className='text-center'>
+            <FaQuestion color='blue'  size={50} />
+            </div>
             <h3 className="text-xl font-semibold mb-2">امتحان 1: ریاضیات</h3>
             <label htmlFor="exam1" className="block mb-2 text-gray-700">
               وارد کردن نمره:
             </label>
+          </div>
             <input
               type="number"
               id="exam1"
               value={scores.exam1}
               onChange={(event) => handleScoreChange(event, "exam1")}
               placeholder="مثال: 20"
-              className="w-full p-2 border border-gray-300 rounded-md"
+              className=" rounded-lg w-[174px] h-[42px] m-2  border border-gray-300 custom-border-gradient"
             />
-          </div>
 
           {/* Exam 2 */}
-          <div className="bg-white shadow-md rounded-md p-6">
+          <div className="bg-white w-[174px] h-[172px] shadow-md mb-4 m-2 rounded-md p-6">
+              <FaQuestion color='blue'  size={50} />
             <h3 className="text-xl font-semibold mb-2">امتحان 2: علوم</h3>
-            <label htmlFor="exam2" className="block mb-2 text-gray-700">
+            <label htmlFor="exam2" className="block  text-gray-700">
               وارد کردن نمره:
             </label>
+          </div>
             <input
               type="number"
               id="exam2"
               value={scores.exam2}
               onChange={(event) => handleScoreChange(event, "exam2")}
               placeholder="مثال: 18"
-              className="w-full p-2 border border-gray-300 rounded-md"
+              className=" rounded-lg w-[174px] h-[42px] m-2 border border-gray-300 custom-border-gradient "
             />
-          </div>
         </main>
       </div>
     </div>
